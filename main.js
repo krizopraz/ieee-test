50f8e9831437ef144cef1fa9c8ac202ea81f9548
Mustafa Değerli <degerlimustafa@protonmail.com>
2023-04-16 23:56:06 +0300

    Haydi kolay gele

---
+++ main.js
@@ -0,0 +1,18 @@
+
+/**
+ * Bir mağaza sitesindeki ürünleri listelemek için bir kod yazmanızı istiyorum.
+ * HTML dosyası içinde bir listede SADECE ürün isimlerinin yazılı olmasını istiyorum
+ * 
+ * @link API documents: https://fakestoreapi.com/docs
+ * @link Ürünlerin çekileceği sırada kullanılacak link: https://fakestoreapi.com/products
+ * @link veri çekme linki: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API,
+ * @link programatik olarak HTML elementi oluşturma https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+ */
+function getProducts(){
+
+
+
+
+}
+
+getProducts()
